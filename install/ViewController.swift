c1e3225e77053169680dc0f6f40d0caa2bae5fcd
Chetan Kapoor <chetankapoor321@gmail.com>
2018-01-10 11:06:40 +0100

    Initial Commit

---
+++ install/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  install
+//
+//  Created by Chetan on 10.01.18.
+//  Copyright © 2018 Chetan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
